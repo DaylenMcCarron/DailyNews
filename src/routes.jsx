65225b1af3d8ce518a911ff5118f11415626cf98
Daylen McCarron <daylenricardomccarron@gmail.com>
2024-02-22 11:14:40 +0530

    add: install libraries and set up routes

---
+++ src/routes.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes, BrowserRouter } from "react-router-dom"
+
+import Home from "./components/home"
+import Header from "./components/header"
+import Contact from "./components/contact"
+
+const Router = () => {
+
+  return (
+    <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="contact" element={<Contact/>} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default Router
