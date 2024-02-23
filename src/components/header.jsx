c5adf4c1b4954ab9e1ffb850ba7f69768feb1653
Daylen McCarron <daylenricardomccarron@gmail.com>
2024-02-23 12:18:23 +0530

    add: Header, json server, Posts reducer

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,27 @@
-
+import { Nav, Navbar } from "react-bootstrap"
+import { LinkContainer } from "react-router-bootstrap"
 
 const Header = () => {
     return (
-        <>Header</>
+        <div className="container">
+            <Navbar className='yellotail'>
+                <LinkContainer to="/">
+                    <Navbar.Brand>Daily News</Navbar.Brand>
+                </LinkContainer>
+            </Navbar>
+            <Nav>
+                <Nav.Item>
+                    <LinkContainer to="/">
+                        <Nav.Link>HOME</Nav.Link>
+                    </LinkContainer>
+                </Nav.Item>
+                <Nav.Item>
+                    <LinkContainer to="contact">
+                        <Nav.Link>CONTACT</Nav.Link>
+                    </LinkContainer>
+                </Nav.Item>
+            </Nav>
+        </div>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Routes from './routes.jsx'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import './styles/main.css'
+
+import { Provider } from 'react-redux'
+import { store } from './store/index.js'
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Provider store={store}>
+
     <Routes />
-  </React.StrictMode>,
+  </Provider>
 )
