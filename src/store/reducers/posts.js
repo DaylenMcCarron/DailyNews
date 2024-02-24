c8e71b3c197a394df318292d109e942fd8b85600
Daylen McCarron <daylenricardomccarron@gmail.com>
2024-02-24 12:43:11 +0530

    add: Thunk

--- src/store/reducers/posts.js
+++ src/store/reducers/posts.js
@@ -1,14 +1,28 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { fetchPosts } from "../utils/thunks";
 
 export const postsSlice = createSlice ({
     name:'posts',
     initialState:{
         loading:true,
-        article:{
+        articles:{
             items:[]
         }
     },
-    reducers:{}
+    reducers:{},
+    extraReducers:(builder)=>{
+        builder
+        .addCase(fetchPosts.pending,(state)=>{
+            state.loading = true;
+        })
+        .addCase(fetchPosts.fulfilled,(state,action)=>{
+            state.articles = action.payload
+            state.loading = false;
+        })
+        .addCase(fetchPosts.rejected,(state)=>{
+            state.loading = false;
+        })
+    }
 })
 
 export default postsSlice.reducer;
